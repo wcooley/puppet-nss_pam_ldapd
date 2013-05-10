519f759a16e519bd10913bf563a1b56755bed3ef
Wil Cooley <wcooley@nakedape.cc>
2013-05-09 23:03:38 -0700

    Add settings to template, parameters of class and hiera

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,16 @@
-class nss_pam_ldapd::config {
+class nss_pam_ldapd::config (
+  $ldap = hiera('nss_pam_ldapd::config::ldap', {
+        uris           => [ 'ldap://localhost', ],
+        basedn         => 'dc=example,dc=com',
+        ssl            => 'start_tls',
+        tls_checkpeer  => 'no',
+        tls_cacertdir  => '/etc/openldap/cacerts',
+        tls_reqcert    => 'never',
+        timelimit      => 120,
+        bind_timelimit => 120,
+        idle_timelimit => 3600,
+      })
+
   file { '/etc/nslcd.conf':
     content => template('nss_pam_ldapd/nslcd.conf.erb'),
     mode    => '0400',

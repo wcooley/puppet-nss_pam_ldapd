5be980a5cef5b518dc4949fb51aeab9317f990b9
Marco Wessel <marco@poop.nl>
2014-03-03 02:40:26 +0100

    add default to case statement

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,6 +2,7 @@ class nss_pam_ldapd::package {
   case $::osfamily {
     redhat: { $packages = ['nss-pam-ldapd'] }
     debian: { $packages = ['libpam-ldapd', 'libnss-ldapd', 'libpam-runtime'] }
+    default: { fail("${::osfamily} is not supported by this module") }
   }
 
 

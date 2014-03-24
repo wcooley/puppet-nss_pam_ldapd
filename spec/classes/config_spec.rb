585ce2b4b2dd182fe5dc488454c5cdf6eab3a303
Wil Cooley <wcooley@nakedape.cc>
2014-03-24 10:58:58 -0700

    Remove checks of nslcd.conf contents
    ... in preparation for converting to Augeas-based configuration.

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -8,10 +8,9 @@ describe 'nss_pam_ldapd::config' do
         :mode => '0400',
         :owner => 'root',
         :group => 'root',
-      })\
-      .with_content(/^uri ldap:\/\/localhost$/)\
-      .with_content(/^base dc=example,dc=com$/)\
-      .with_content(/^ssl start_tls$/)\
+      })
+    }
+
     }
   end
 
@@ -28,16 +27,10 @@ describe 'nss_pam_ldapd::config' do
       }
     end
 
-    it {
-      should contain_file('/etc/nslcd.conf').with({
-        :mode => '0400',
-        :owner => 'root',
-        :group => 'root',
-      })\
-      .with_content(/^uri ldap:\/\/ldap1\.example\.com ldap:\/\/ldap2\.example\.com$/)\
-      .with_content(/^base o=example org,c=us$/)\
-      .with_content(/^ssl broken$/)\
-      .with_content(/^tls_reqcert always$/)
+    it { should contain_file('/etc/nslcd.conf') \
+        .with_mode('0400')  \
+        .with_owner('root') \
+        .with_group('root')
     }
 
   end

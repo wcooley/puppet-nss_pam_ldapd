1c37862fa074cdc450a50424f9cd023b1dab3fb7
Wil Cooley <wcooley@nakedape.cc>
2014-03-24 11:54:14 -0700

    Implement all params non-default

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,19 +13,65 @@ class nss_pam_ldapd::config (
   $template = 'nss_pam_ldapd/nslcd.conf.erb'
   ) {
 
-  $ldap_uri_val = join($ldap['uris'], ' ')
+  if has_key($ldap, 'uris') {
+    $ldap_uri_val = join($ldap['uris'], ' ')
+    $aug_uri = "set uri '${ldap_uri_val}'"
+  }
+  else {
+    $aug_uri = undef
+  }
 
-  $augeas_changes = [
-      "set uri '${ldap_uri_val}'",
-      "set basedn '${ldap[basedn]}'",
-      "set ssl '${ldap[ssl]}'",
-      "set tls_checkpeer '${ldap[tls_checkpeer]}'",
-      "set tls_cacertdir '${ldap[tls_cacertdir]}'",
-      "set tls_reqcert '${ldap[tls_reqcert]}'",
-      "set timelimit '${ldap[timelimit]}'",
-      "set bind_timelimit '${ldap[bind_timelimit]}'",
-      "set idle_timelimit '${ldap[idle_timelimit]}'",
-  ]
+  $aug_basedn = has_key($ldap, 'basedn') ? {
+        true => "set basedn '${ldap['basedn']}'",
+        default => undef,
+      }
+
+  $aug_ssl = has_key($ldap, 'ssl') ? {
+        true    => "set ssl '${ldap['ssl']}'",
+        default => undef,
+      }
+
+  $aug_tls_checkpeer = has_key($ldap, 'tls_checkpeer') ? {
+        true => "set tls_checkpeer '${ldap['tls_checkpeer']}'",
+        default => undef,
+      }
+
+  $aug_tls_cacertdir = has_key($ldap, 'tls_cacertdir') ? {
+        true    => "set tls_cacertdir '${ldap['tls_cacertdir']}'",
+        default => undef,
+      }
+
+  $aug_tls_reqcert = has_key($ldap, 'tls_reqcert') ? {
+        true    => "set tls_reqcert '${ldap['tls_reqcert']}'",
+        default => undef,
+      }
+
+  $aug_timelimit = has_key($ldap, 'timelimit') ? {
+        true    => "set timelimit '${ldap['timelimit']}'",
+        default => undef,
+      }
+
+  $aug_bind_timelimit = has_key($ldap, 'bind_timelimit') ? {
+        true    => "set bind_timelimit '${ldap['bind_timelimit']}'",
+        default => undef,
+      }
+
+  $aug_idle_timelimit = has_key($ldap, 'idle_timelimit') ? {
+        true    => "set idle_timelimit '${ldap['idle_timelimit']}'",
+        default => undef,
+      }
+
+  $augeas_changes = grep([
+      $aug_uri,
+      $aug_basedn,
+      $aug_ssl,
+      $aug_tls_checkpeer,
+      $aug_tls_cacertdir,
+      $aug_tls_reqcert,
+      $aug_timelimit,
+      $aug_bind_timelimit,
+      $aug_idle_timelimit,
+  ], '.')
 
   file { '/etc/nslcd.conf':
     mode    => '0400',

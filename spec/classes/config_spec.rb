32dee1401adf0b58203b385f3ddf7de5434e81d9
Wil Cooley <wcooley@nakedape.cc>
2014-03-24 11:27:25 -0700

    Update non-defaults test for augeas config

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -26,23 +26,43 @@ describe 'nss_pam_ldapd::config' do
     }
   end
 
-  context 'non-default' do
-    let(:facts) {{ :osfamily => 'RedHat' }}
+  context 'All params non-default' do
+    let(:facts) {{ :osfamily => 'redhat' }}
 
-    let(:params) do {
-        :ldap => {
-          'uris' => ['ldap://ldap1.example.com', 'ldap://ldap2.example.com' ],
-          'basedn' => 'o=example org,c=us',
-          'ssl' => 'broken',
-          'tls_reqcert' => 'always',
-        }
-      }
-    end
+    lp = {
+      'uris'           => ['ldap://ldap1.example.com',
+                           'ldap://ldap2.example.com' ],
+      'basedn'         => 'o=example org,c=us',
+      'ssl'            => 'broken',
+      'tls_checkpeer'  => 'yes',
+      'tls_cacertdir'  => '/etc/pki/tls',
+      'tls_reqcert'    => 'always',
+      'timelimit'      => '90',
+      'bind_timelimit' => '15',
+      'idle_timelimit' => '1800',
+    }
+
+    let(:params) {{ :ldap => lp }}
 
     it { should contain_file('/etc/nslcd.conf') \
         .with_mode('0400')  \
         .with_owner('root') \
         .with_group('root')
+
+    }
+
+    it { should contain_augeas('/etc/nslcd.conf') \
+      .with_changes([
+                      %Q<set uri '#{lp['uris'].join(' ')}'>,
+                      %Q<set basedn '#{lp['basedn']}'>,
+                      %Q<set ssl '#{lp['ssl']}'>,
+                      %Q<set tls_checkpeer '#{lp['tls_checkpeer']}'>,
+                      %Q<set tls_cacertdir '#{lp['tls_cacertdir']}'>,
+                      %Q<set tls_reqcert '#{lp['tls_reqcert']}'>,
+                      %Q<set timelimit '#{lp['timelimit']}'>,
+                      %Q<set bind_timelimit '#{lp['bind_timelimit']}'>,
+                      %Q<set idle_timelimit '#{lp['idle_timelimit']}'>,
+      ])
     }
 
   end
